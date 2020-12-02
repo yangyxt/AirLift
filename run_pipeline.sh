f44853d6a26e7140ede42ed5d2bc849cf64c0233
yangyxt <u3005579@connect.hku.hk>
2020-12-02 18:16:40 +0800

    change path

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -247,8 +247,8 @@ function extract_reads_with_parallel(){
     for i in `ls ${OUTPUT}/*.chain`; do 
         local chain_basename=`basename $i | sed s/.chain//`
         cat <(bwa mem -R "@RG\tID:${SAMPLE}\tSM:${SAMPLE}\tPL:illumina\tLB:${SAMPLE}" ${NEWREF} ${FIRST_PAIR} ${SECOND_PAIR} | \
-        samtools view -H -) <(/usr/bin/time -v -p -o "${OUTPUT}/bedfiles/${chr}/constant/constant_liftOver.time" liftOver -minMatch=1 <(samtools view -h -L "${OUTPUT}/bedfiles/${chain_basename}/constant/constant_regions.bed" ${READ_BAM} | bamToBed -i -) ${i} >("${SRCDIR}/5-merge/liftBedToSam" <(samtools view -L "${OUTPUT}/bedfiles/${chain_basename}/constant/constant_regions.bed" ${READ_BAM}) - 4 3,4 1,2) "${OUTPUT}/bedfiles/${chain_basename}/constant/constant_unmapped.bed") | \
-        samtools sort -l5 > ${OUTPUT}/bedfiles/${chain_basename}/constant/constant_lifted.bam
+        samtools view -H -) <(/usr/bin/time -v -p -o "${OUTPUT}/bedfiles/${chr}/constant_bed/constant_liftOver.time" liftOver -minMatch=1 <(samtools view -h -L "${OUTPUT}/bedfiles/${chain_basename}/constant_bed/constant_regions.bed" ${READ_BAM} | bamToBed -i -) ${i} >("${SRCDIR}/5-merge/liftBedToSam" <(samtools view -L "${OUTPUT}/bedfiles/${chain_basename}/constant_bed/constant_regions.bed" ${READ_BAM}) - 4 3,4 1,2) "${OUTPUT}/bedfiles/${chain_basename}/constant_bed/constant_unmapped.bed") | \
+        samtools sort -l5 > ${OUTPUT}/bedfiles/${chain_basename}/constant_bed/constant_lifted.bam
     done
 
     for i in `ls ${OUTPUT}/*.chain`; do
